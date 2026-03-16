25fa6e0f011398544cf377a79bfb25317170759d
caiodideuscorrea27-cmyk <caiodideuscorrea27@gmail.com>
2026-03-16 15:47:20 -0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-nome = input("Digite seu nome: ")
-hobbie = input("Digite seu hobbie: ")
-idade = input("Digite sua idade:")
-peso = input("Digite seu peso:")
-
-print(f"olá {nome}")
-print(f"amei o seu saber que vc é bom em {hobbie}")
-print(f"você tem {idade} e pesa {peso} kg")
-print(f"ficando gorda em cuidado, {nome}")
